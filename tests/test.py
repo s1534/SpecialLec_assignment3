3e62421dc4909fd7f9fdea01387ee92996d6d530
mishima-mac <s1534onct@gmail.com>
2022-11-27 14:45:29 +0900

    change test.py

--- tests/test.py
+++ tests/test.py
@@ -3,20 +3,20 @@ from speciallecture.sample import CSVPrinter
 
 class TestCSVPrinter(unittest.TestCase):
     def test_read1(self):
-        file = 'data.csv'
+        file = 'tests/data.csv'
         printer = CSVPrinter(file)
         line = printer.read()
         self.assertEqual(3, len(line))
 
     def test_read2(self):
-        file = 'data.csv'
+        file = 'tests/data.csv'
         printer = CSVPrinter(file)
         line = printer.read()
         self.assertEqual("value2B", line[1][1])
 
     def test_read3(self):
         try:
-            file = 'none.csv'
+            file = 'tests/none.csv'
             printer = CSVPrinter(file)
             printer.read()
             unittest.TestCase.fail("This line should not be invoked")
